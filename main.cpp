228159368c2fb1f83eff5f213e09d22a91b6bc4e
Hanchao Chen <56032754+acse-hc1119@users.noreply.github.com>
2020-02-24 21:48:52 +0000

    Boundary initialization

---
+++ main.cpp
@@ -0,0 +1,86 @@
+//
+//  SPH_Snippet.cpp
+//  AWE
+//
+//  Created by HANCHAO CHEN on 2020/2/24.
+//  Copyright © 2020 Hanchao Chen. All rights reserved.
+//
+
+#include "SPH_2D.hpp"
+//#include "file_writer.h"
+
+SPH_main domain;
+
+// the repulsive force's range
+double frange = domain.dx;
+
+
+// push back a particle
+void pushback(SPH_particle *part)
+{
+    if (part->x[0] <= 0.0)
+    {
+        part->x[0] = -part->x[0] + frange;
+    }
+    
+    if (part->x[0] >= 20.0)
+    {
+        part->x[0] = 2*20.0 - frange;
+    }
+    
+    if (part->x[1] <= 0.0)
+    {
+        part->x[1] = -part->x[1] + frange;
+    }
+    
+    if (part->x[1] >= 10.0)
+    {
+        part->x[1] = 2*10.0-part->x[1] - frange;
+    }
+}
+
+
+int main(void)
+{
+    domain.set_values(); //Set simulation parameters
+    domain.initialise_grid(); //initialise simulation grid
+
+    // set up boundary
+    int thick = int(2*domain.h/domain.dx);
+    
+    double min_bottom[2] = {-thick*domain.dx, -thick*domain.dx};
+    double max_bottom[2] = {20.0+thick*domain.dx, -domain.dx};
+    
+    double min_left[2] = {-thick*domain.dx, 0.0};
+    double max_left[2] = {-domain.dx, 10.0};
+    
+    double min_right[2] = {20.0+domain.dx, 0.0};
+    double max_right[2] = {20.0+thick*domain.h, 10.0};
+    
+    double min_top[2] = {-thick*domain.dx, 10.0+domain.dx};
+    double max_top[2] = {20.0+thick*domain.dx, 10.0+thick*domain.dx};
+    
+    // set up fluid
+    double min_x1[2] = {0.0, 0.0};
+    double max_x1[2] = {20.0, 2.0};
+    
+    double min_x2[2] = {0.0, 2.0+domain.dx};
+    double max_x2[2] = {3.0, 5.0};
+    
+    
+    domain.place_points(min_bottom,max_bottom);                //places initial points - will need to be modified to include boundary points and the specifics of where the fluid is in the domain
+    domain.place_points(min_left,max_left);
+    domain.place_points(min_right,max_right);
+    domain.place_points(min_top,max_top);
+    
+    domain.place_points(min_x1,max_x1);
+    domain.place_points(min_x2,max_x2);
+    
+    domain.allocate_to_grid();                                    //needs to be called for each time step
+
+    domain.neighbour_iterate(&domain.particle_list[125]);        //finding all the neighbours of the 100th particle in the list - in reality the simulation loop will need to do the calculations for the neighbours of every particle
+
+    // write_file("example.vtp", &domain.particle_list);
+    
+    return 0;
+}
