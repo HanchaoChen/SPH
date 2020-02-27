0fe3c83fe209d96b53dd12fd9230d8ce4478c569
ww819 <ww819@ic.ac.uk>
2020-02-27 15:04:50 +0000

    add comment for file writer

--- includes/file_writer.h
+++ includes/file_writer.h
@@ -3,7 +3,8 @@
 #include <vector>
 #include "SPH_2D.h"
 
-void filename_padLeft(string& file_num, const size_t size = 8, const char padzero = '0');
+void filename_padLeft(string& file_num, const size_t size = 8,
+	       const char padzero = '0');
 
 int write_file(int& iter_num,
 	       std::vector<SPH_particle> *particle_list, bool test = false);
