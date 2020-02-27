955f0f0bdeb7c186fb7e89d9431b6adeb498e378
ww819 <ww819@ic.ac.uk>
2020-02-27 02:06:48 +0000

    add pythontest

--- tests/python_tests.py
+++ tests/python_tests.py
@@ -1,13 +1,45 @@
 import vtk
+import os
+import numpy as np
 
-# def test_velocity():
-#     reader = vtk.vtkXMLPolyDataReader()
-#     for i in range
-#     reader.SetFileName('tests/test_file_writer.vtp')
-#     reader.Updata()
+def test_position():
+    reader = vtk.vtkXMLPolyDataReader()
+    path = "data/"
+    #path2 = "../data/"
+    files = os.listdir(path)
+    for file in files:
+            if not os.path.isdir(file):
+                 #f = open(path + "/" + file) 
+                 reader.SetFileName(path + file)
+                 reader.Update()
+                 pdata = reader.GetOutput()
+                 points = pdata.GetNumberOfPoints()
+                 x = np.zeros(points)
+                 y = np.zeros(points)
+                 z = np.zeros(points)
+                 for i in range(0, points):
+                         x[i],y[i],z[i] = pdata.GetPoint(i)
+                         assert ((x[i] <= 20.4) and (x[i] >= -0.4))
+                         assert ((y[i] <= 10.4) and (y[i] >= -0.4))
+                         assert (z[i] == 0.0)
+                                
+def test_density():
+    reader = vtk.vtkXMLPolyDataReader()
+    path = "data/"
+    #path2 = "../data/"
+    files = os.listdir(path)
+    #files = os.listdir(path2)
+    for file in files:
+            if not os.path.isdir(file):
+                 #f = open(path + "/" + file) 
+                 reader.SetFileName(path + file)
+                 #reader.SetFileName(path2 + file)
+                 reader.Update()
+                 pdata = reader.GetOutput()
+                 points = pdata.GetNumberOfPoints()
+                 for i in range(0, points):
+                          assert (pdata.GetPointData().GetArray('Density').GetValue(i) > 0.0)
 
-#     pdata = reader.GetOutput()
-#     assert 
 
 
 def test_file_writer_output():
@@ -24,3 +56,5 @@ def test_file_writer_output():
     assert (pdata.GetPointData().GetArray('Pressure').GetValue(5)
             == 5.0)
     
+#test_position()
+test_density()
