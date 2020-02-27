c887eb229c467d48267aad89bd42d90ff42372e5
acse-gel19 <56037100+acse-gel19@users.noreply.github.com>
2020-02-27 11:43:38 +0000

    with predictor-corrector

--- includes/SPH_2D.h
+++ includes/SPH_2D.h
@@ -2,22 +2,41 @@
 #include <vector>
 #include <cmath>
 #include <iostream>
+#include <math.h>
+#include <algorithm>
+
+const double g = 9.8;
+const double rho_0 = 1000.0;
+const double ga = 7;
+const double C0 = 20; // maybe should change this later
+const double Pi = 3.14;
+const double miu = 0.001;
+const double B = rho_0 * C0 * C0 / ga;
 
 using namespace std;
 
 class SPH_main;
 
+
 class SPH_particle
 {
 public:
+
 	double x[2], v[2];				//position and velocity
 	double rho, P;					//density and pressure
+	double dedv[2]; // dVdT
+	double drho;  // drhodt
 
 	static SPH_main *main_data;		//link to SPH_main class so that it can be used in calc_index
 
-	int list_num[2];				//index in neighbour finding array
-
+	int list_num[2];	//index in neighbour finding array
+	
+	void set_particle_deri(void);
+	void init_particle();
 	void calc_index(void);
+	void cal_P(void);       //calculate the pressure of particle
+
+
 };
 
 
@@ -33,11 +52,36 @@ public:
 
 	void allocate_to_grid(void);			//allocates all the points to the search grid (assumes that index has been appropriately updated)
 
+	void cal_derivative(SPH_particle *part);      // calculate the derivative
+	//void cal_derivative_second(SPH_particle* part); // calculate the derivative
+
+
+	void smooth_density(SPH_particle* part, int t_step);      // smooth the denstity every 10 to 20 times
+
 	void neighbour_iterate(SPH_particle *part);
+	
+	// void update_parameters_fe(int step, double dt);
+	//void update_parameters_fe_second(int step,double dt);
+	void update_parameters_pc(double dt, int step);
+
+	void get_cfl_time_step(double* part_v, double* other_part_v);
+	void get_tf_ta_time_step(double rho, double *dedv);
+
+	void update_min_time_step();
+	//void pushback(SPH_particle* part,double dt);
+	//void pushback_position(SPH_particle* part);
+	//double rforce(double r);
+
+	int min_t_cfl, min_tf, min_ta; // Used in adaptive timestepping, default to long value
+	int max_dt;
+	int vij; //used to temporarily store the difference in velocity between particle i and j
 
 	double h;								//smoothing length
 	double h_fac;
 	double dx;								//particle initial spacing
+	double frange;
+	double m;
+
 
 	double min_x[2], max_x[2];				//dimensions of simulation region
 
@@ -46,4 +90,5 @@ public:
 	vector<SPH_particle> particle_list;						//list of all the particles
 
 	vector<vector<vector<SPH_particle*> > > search_grid;		//Outer 2 are the grid, inner vector is the list of pointers in each cell
-};
+
+	};
